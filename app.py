1860f7cbd9f7a9508ad2058e922cb3d1ea6531e1
aprokid <67274126+aprokid@users.noreply.github.com>
2022-04-01 22:12:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+class Wallet:
+    def __init__(self):
+        random = Crypto.Random.new()
+        self._private_key = RSA.generate(1024, random)
+        self._public_key = self._private_key.public_key()
+
+    @property
+    def identity(self):
+        pubkey = binascii.hexlify(self._public_key.exportKey(format='DER'))
+        return pubkey.decode('ascii')   
+
 # if __name__ == '__main__':
 #   # myWallet = Wallet()
 #   # blockchain = Blockchain()
